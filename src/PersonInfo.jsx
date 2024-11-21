abd138fdaf11b05cefa27952ed22141b76d7a9ca
Syrgak13 <syrgaktabyldiev13@gmail.com>
2024-11-21 14:52:11 +0600

    add PersonInfo component to display user details

--- src/PersonInfo.jsx
+++ src/PersonInfo.jsx
@@ -1,29 +1,9 @@
-import React from 'react';
-
 const PersonInfo = () => {
-    const person = {
-        name: "Syrgak Tabyldiev",
-        position: "Junior Developer",
-        company: "Ala-Too University",
-        details: [
-            "Passionate about learning modern web technologies.",
-            "Skilled in Java, Python, and Graphic Design.",
-            "Ambition to excel in the tech industry.",
-        ],
-    };
 
     return (
         <div>
-            <h1>{person.name}</h1>
-            <p><strong>Position:</strong> {person.position}</p>
-            <p><strong>Company:</strong> {person.company}</p>
-            <ul>
-                {person.details.map((detail, index) => (
-                    <li key={index}>{detail}</li>
-                ))}
-            </ul>
+
+            <button>Show Message</button>
         </div>
     );
 };
-
-export default PersonInfo;
